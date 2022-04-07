1d147172ab0855975da44ac2955605db85139b5c
ggruszczynski <ggruszczynski@gmail.com>
2022-04-07 16:36:04 +0200

    memusage

--- lab20_explicit_implicit_iterative_solvers/memory_usage.py
+++ lab20_explicit_implicit_iterative_solvers/memory_usage.py
@@ -1,16 +1,37 @@
 import numpy as np
-from numpy.linalg import inv
 import psutil
-# arr = np.ones((170_000,), dtype=np.uint8)
 
-n = 5000
-M = np.random.rand(n,n)
-Minv = inv(M)
+import scipy.sparse.linalg
+from scipy.sparse import csc_matrix
 
-np.allclose(np.dot(M, Minv), np.eye(n))
+np.random.seed(0)  
 
-# mem = psutil.virtual_memory()
-# print(mem)
+n = 3000
+rhs = np.array(np.random.rand(n), dtype=np.float64)
+
+# mtx = np.random.rand(n,n) 
+mtx = scipy.sparse.random(n, n, density=0.01, dtype = np.float64).toarray()
+
+# Direct method of solving system of linear equations
+# https://developer.apple.com/documentation/accelerate/solving_systems_of_linear_equations_with_lapack
+# https://stackoverflow.com/questions/31256252/why-does-numpy-linalg-solve-offer-more-precise-matrix-inversions-than-numpy-li
+# mtx_inv = scipy.linalg.inv(mtx)
+# np.allclose(np.dot(mtx, mtx_inv), np.eye(n))
+# x = np.dot(mtx, rhs)
+
+x = scipy.linalg.solve(mtx, rhs)
+# np.allclose(np.dot(mtx, x), rhs)
+
+# Now an iterative method
+# https://scipy-lectures.org/advanced/scipy_sparse/solvers.html#iterative-solvers
+# https://stackoverflow.com/questions/15755270/scipy-sparse-matrices-purpose-and-usage-of-different-implementations
+# csc_matrix - converts the matrix to Compressed Sparse Column (CSC) format
+# x = scipy.sparse.linalg.bicg(csc_matrix(mtx), rhs) # check the performance loss without converting to csc matrix
+# x = scipy.sparse.linalg.bicg(mtx, rhs)
+# x = scipy.sparse.linalg.gmres(csc_matrix(mtx), rhs)
+
+
+
 
 # https://en.wikipedia.org/wiki/Resident_set_size
 # In computing, resident set size (RSS) is the portion of memory occupied by a process 
@@ -19,5 +40,8 @@ np.allclose(np.dot(M, Minv), np.eye(n))
 # either because some parts of the occupied memory were paged out, 
 # or because some parts of the executable were never loaded.
 
-# psutil.Process().memory_info().rss  # in bytes 
-print(f"Memory used by process: {psutil.Process().memory_info().rss/(1024*1024)} [MB]")
+print(f"Memory used by process: {psutil.Process().memory_info().rss/(1024*1024)} [MB]")  # convert from bytes to MB 
+
+# print(psutil.virtual_memory())
+
+#$ time python3 memory_usage.py
